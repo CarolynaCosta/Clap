7cd887815dcc7b3f3eae2d853093dd09057de993
sarazoli <sarinhacem@gmail.com>
2025-11-06 19:42:53 -0300

    cadastro e login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,5 +9,13 @@ export const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },  {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
   },
+  {
+    path: 'cadastro',
+    loadComponent: () => import('./cadastro/cadastro.page').then( m => m.CadastroPage)
+  },
+
 ];
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,28 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// src/environments/firebase.config.ts
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyASMoY0vTrNgFjyGkmQfQw4-YrFPfHnN0w",
+    authDomain: "clap-c767f.firebaseapp.com",
+    projectId: "clap-c767f",
+    storageBucket: "clap-c767f.firebasestorage.app",
+    messagingSenderId: "836484119429",
+    appId: "1:836484119429:web:58da07d914be3ecfe8e2ec"
+  }
+
+
 };
+
+const app = initializeApp(environment.firebaseConfig); // Inicializa o Firebase
+export const auth = getAuth(app); // Aquivai usar para login, cadastro, logout
+export const db = getFirestore(app);// Aqui você salva ou busca dados do usuário
+
 
 /*
  * For easier debugging in development mode, you can import the following file
